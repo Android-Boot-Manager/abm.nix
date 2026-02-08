cfe68f921c12f901964cbb690bee27c53e98f7cc
Dom Rodriguez <shymega@shymega.org.uk>
2026-02-08 00:25:49 +0000

    feat: Initial commit

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Flake with NixOS configurations for ABM Infrastructure";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.11";
+    blueprint = {
+      url = "github:numtide/blueprint";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs: inputs.blueprint { inherit inputs; };
+}
